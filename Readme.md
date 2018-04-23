3aa887e16264fb01670b6bb96ed55e9ea5741d73
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# How to enable/disable command buttons on the client side
+
+
+<p>The example demonstrates how to enable/disable command buttons for the ASPxGridView using the client-side methods only. The main idea of the example is to implement custom command buttons and define their client-side IDs on the server side.</p><p><strong>See Also:</strong><br />
+<a href="https://www.devexpress.com/Support/Center/p/K18282">The general technique of using the Init/Load event handler</a><br />
+<a href="https://www.devexpress.com/Support/Center/p/E1246">How to create a custom command button with the appearance and action depending on a row state</a><br />
+<a href="https://www.devexpress.com/Support/Center/p/E1522">How Insert, Update and Delete ASPxGridView's records with buttons</a></p>
+
+<br/>
+
+
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,80 @@
+Imports Microsoft.VisualBasic
+Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports DevExpress.Web.ASPxEditors
+Imports DevExpress.Web.ASPxGridView
+
+Partial Public Class _Default
+	Inherits System.Web.UI.Page
+	Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+
+	End Sub
+
+	' Init 
+
+	Protected Sub lnkEdit_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim lnk As ASPxHyperLink = TryCast(sender, ASPxHyperLink)
+		Dim container As GridViewDataItemTemplateContainer = TryCast(lnk.NamingContainer, GridViewDataItemTemplateContainer)
+
+        lnk.ClientInstanceName = String.Format("lnkEdit{0}", container.VisibleIndex)
+
+        If (String.IsNullOrEmpty(container.Grid.ClientInstanceName)) Then
+            lnk.ClientSideEvents.Click = String.Format("function (s, e) {{ {0}.StartEditRow ({1}); }}", container.Grid.ClientID, container.VisibleIndex)
+        Else
+            lnk.ClientSideEvents.Click = String.Format("function (s, e) {{ {0}.StartEditRow ({1}); }}", container.Grid.ClientInstanceName, container.VisibleIndex)
+        End If
+    End Sub
+
+	Protected Sub lnkNew_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim lnk As ASPxHyperLink = TryCast(sender, ASPxHyperLink)
+		Dim container As GridViewDataItemTemplateContainer = TryCast(lnk.NamingContainer, GridViewDataItemTemplateContainer)
+
+		lnk.ClientInstanceName = String.Format("lnkNew{0}", container.VisibleIndex)
+
+        If (String.IsNullOrEmpty(container.Grid.ClientInstanceName)) Then
+            lnk.ClientSideEvents.Click = String.Format("function (s, e) {{ {0}.AddNewRow (); }}", container.Grid.ClientID)
+        Else
+            lnk.ClientSideEvents.Click = String.Format("function (s, e) {{ {0}.AddNewRow (); }}", container.Grid.ClientInstanceName)
+        End If
+    End Sub
+
+	Protected Sub lnkDelete_Init(ByVal sender As Object, ByVal e As EventArgs)
+		Dim lnk As ASPxHyperLink = TryCast(sender, ASPxHyperLink)
+		Dim container As GridViewDataItemTemplateContainer = TryCast(lnk.NamingContainer, GridViewDataItemTemplateContainer)
+
+		lnk.ClientInstanceName = String.Format("lnkDelete{0}", container.VisibleIndex)
+
+        If (String.IsNullOrEmpty(container.Grid.ClientInstanceName)) Then
+            lnk.ClientSideEvents.Click = String.Format("function (s, e) {{ {0}.DeleteRow ({1}); }}", container.Grid.ClientID, container.VisibleIndex)
+        Else
+            lnk.ClientSideEvents.Click = String.Format("function (s, e) {{ {0}.DeleteRow ({1}); }}", container.Grid.ClientInstanceName, container.VisibleIndex)
+        End If
+    End Sub
+
+	' Load 
+
+	Protected Sub lnkEdit_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim lnk As ASPxHyperLink = TryCast(sender, ASPxHyperLink)
+
+		lnk.ClientEnabled = Not chkEdit.Checked
+	End Sub
+
+	Protected Sub lnkNew_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim lnk As ASPxHyperLink = TryCast(sender, ASPxHyperLink)
+
+		lnk.ClientEnabled = Not chkNew.Checked
+	End Sub
+
+	Protected Sub lnkDelete_Load(ByVal sender As Object, ByVal e As EventArgs)
+		Dim lnk As ASPxHyperLink = TryCast(sender, ASPxHyperLink)
+
+		lnk.ClientEnabled = Not chkDelete.Checked
+	End Sub
+End Class
